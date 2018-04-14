c09d8b8a699ef7043b6a8be06c532f5257433411
Çağdaş Işık <cyasam@gmail.com>
2018-04-14 18:38:27 +0300

    Some configurations changed.

--- config/consts.js
+++ config/consts.js
@@ -3,5 +3,5 @@ const { PORT } = require('./keys');
 module.exports = {
   AUTH_EXPIRES_IN: 60 * 60 * 2,
   UPLOADS_FOLDER: 'public/uploads',
-  UPLOADS_URL: `//192.168.1.13:${PORT}/uploads`
+  UPLOADS_URL: `//localhost:${PORT}/uploads`
 };

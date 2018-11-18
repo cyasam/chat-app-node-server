9a76e1e5598c26ec453a00a8adc4c97e488befd5
cyasam <cyasam@gmail.com>
2018-11-19 00:29:09 +0300

    Profile Image url fix.

--- config/consts.js
+++ config/consts.js
@@ -1,10 +1,6 @@
-const {
-  PORT
-} = require('./keys');
-
 module.exports = {
   AUTH_EXPIRES_IN: 60 * 60 * 2,
   UPLOADS_FOLDER: 'public/uploads',
-  UPLOADS_URL: `//localhost:${PORT}/uploads`,
+  UPLOADS_URL: `/uploads`,
   APP_BASE_URL: process.env.NODE_ENV === 'production' ? 'https://chat-app-node-server.herokuapp.com' : 'http://localhost:9999'
 };
